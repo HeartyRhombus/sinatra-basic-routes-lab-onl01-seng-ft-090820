5579b7c426f12b762988b496775184cc82fd61f6
HeartyRhombus <kaylahmg@gmail.com>
2020-10-11 15:09:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Kayla"
+    end
+
+    get '/hometown' do
+        "My hometown is Kent, WA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'I Put A Spell On You' by Bette Midler"
+    end
 end
